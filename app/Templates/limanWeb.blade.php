20c925f3512a1b9c15164be1b2e3ec2ec8be3ade
Murat Alkan <muratalkanw@hotmail.com>
2021-06-24 18:46:50 +0000

    Add template web page

---
+++ app/Templates/limanWeb.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+     <head>
+        <title>Liman MYS Template Page</title>
+        <style>
+            body {
+                width: 35em;
+                margin: 0 auto;
+                font-family: Tahoma, Verdana, Arial, sans-serif;
+            }
+        </style>
+     </head>
+     <body>
+        <h1>Liman MYS Template Web Page</h1>
+        <p>Eğer bu sayfayı görüyorsanız, nginx ayarları ve domain ekleme işlemi başarılıdır.</p>
+        <p>Daha fazla bilgi için:
+        <a href="https://www.liman.dev/">liman.dev</a><br/>
+     </body>
+</html>
