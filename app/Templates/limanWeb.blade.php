b766c067cc881f0debd4430275880b5a70cb3c13
Murat Alkan <muratalkanw@hotmail.com>
2021-06-24 19:39:15 +0000

    Update default web page

--- app/Templates/limanWeb.blade.php
+++ app/Templates/limanWeb.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
      <head>
-        <title>Liman MYS Template Page</title>
+        <title>Liman Default Web Page</title>
         <style>
             body {
                 width: 35em;
@@ -11,7 +11,7 @@
         </style>
      </head>
      <body>
-        <h1>Liman MYS Template Web Page</h1>
+        <h1>Liman MYS Default Web Page</h1>
         <p>Eğer bu sayfayı görüyorsanız, nginx ayarları ve domain ekleme işlemi başarılıdır.</p>
         <p>Daha fazla bilgi için:
         <a href="https://www.liman.dev/">liman.dev</a><br/>
